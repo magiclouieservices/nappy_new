eba73b91bff3cc71e1affbd1602eee259ba85b92
Louie Tugas <magiclouie2gas@gmail.com>
2023-07-30 00:57:50 +0800

    Made header sticky
    Implemented sticky header functionality for improved user navigation. Added Tailwind CSS classes 'sticky', 'top-0', 'z-50', and 'bg-white' to the header div in root.html.heex. The updated div now looks like this: <div class="border-b border-slate-20 sticky top-0 z-50 bg-white">. Please review and merge if acceptable.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       ...defaultTheme.screens,
     },
     extend: {
+      position: ['sticky'], // Added position property for sticky
       screens: {
         'md': '740px',
         'lg': '1000px',
